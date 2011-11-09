ee431ebc409c47e22f3af4762ca94bdd42e77dae
annegentle <anne@openstack.org>
2011-11-09 14:45:12 -0600

    Updates for auth and EC2 creds and a start at IP address association
    Change-Id: Iabd272c2447e03c42450275e3d4905973fef1650

--- doc/src/docbkx/api-quick-start/target/docbkx/webhelp/api-quick-start/content/search/htmlFileInfoList.js
+++
@@ -1,7 +0,0 @@
-fil = new Array();
-fil["0"]= "deleteme.html@@@null@@@null";
-fil["1"]= "Getting-Credentials-a00665.html@@@2. Getting Credentials@@@null";
-fil["2"]= "Launching-Images-a09140.html@@@5. Listing Images@@@null";
-fil["3"]= "Listing-Images-a01518.html@@@6. Launching Instances@@@null";
-fil["4"]= "Sending-Requests-to-API-a09879.html@@@3. Sending Requests to the API@@@null";
-fil["5"]= "Setting-Up-python-novaclient-a03867.html@@@4. Setting Up python-novaclient@@@null";
--- doc/src/docbkx/api-quick-start/target/docbkx/webhelp/api-quick-start/content/search/htmlFileList.js
+++
@@ -1,8 +0,0 @@
-//List of files which are indexed.
-fl = new Array();
-fl["0"]= "deleteme.html";
-fl["1"]= "Getting-Credentials-a00665.html";
-fl["2"]= "Launching-Images-a09140.html";
-fl["3"]= "Listing-Images-a01518.html";
-fl["4"]= "Sending-Requests-to-API-a09879.html";
-fl["5"]= "Setting-Up-python-novaclient-a03867.html";
