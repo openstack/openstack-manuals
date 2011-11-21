a184a65e067411c4d7ef552673ade63040bf39b1
Lorin Hochstein <lorin@isi.edu>
2011-11-20 19:06:04 -0500

    Removed and ignored auto-generated files.
    Remoed all files in target/ subdirectories.
    
    Fixed .gitignore so it properly ignores these files.
    
    Change-Id: Ib435a9cc3d858081717d838e48528a96239e2e9d

--- doc/src/docbkx/openstack-object-storage-admin/target/docbkx/webhelp/os-objectstorage-adminguide/common/comments.js
+++
@@ -1,7 +0,0 @@
-              var disqus_url = window.location.toString().replace(/http\:\/\/docs\.rackspace(cloud)?\.com\//,"http://docs.rackspace.com/");
-              var disqus_title = document.title;
-	      (function() {
-	      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-	      dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-	      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-	      })();
