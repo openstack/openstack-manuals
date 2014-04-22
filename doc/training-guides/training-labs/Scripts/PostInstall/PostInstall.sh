631091bc1747b1ceea8691e8ba1e1f25c1d6f1c0
Roger Luethi <rl@patchworkscience.org>
2014-04-22 16:39:33 +0200

    Read values in PostInstall.sh
    Fix some syntax errors and read another variable (Role_ID)
    which is used for keystone user-role-add.
    
    Change-Id: Iace2cce9725c7b0ac46532ed46c08233072c8ffc

--- doc/training-guides/training-labs/Scripts/PostInstall/PostInstall.sh
+++ doc/training-guides/training-labs/Scripts/PostInstall/PostInstall.sh
@@ -16,13 +16,16 @@
 keystone tenant-create --name Os_Training
 
 echo "Enter Tenant ID"
-read $Tenant_ID
+read Tenant_ID
 
 #Create User and assign required role
 keystone user-create --name=trainee --pass=cloud --tenant-id $Tenant_ID --email=user_one@domain.com
 echo "Enter User ID"
-read $User_ID
-keystone user-role-add --tenant-id $Tenant_ID  --user-id $User_ID --role-id $role_id
+read User_ID
+keystone role-list
+echo "Enter Role ID"
+read Role_ID
+keystone user-role-add --tenant-id $Tenant_ID  --user-id $User_ID --role-id $Role_ID
 
 # Create Networks
 quantum net-create --tenant-id $Tenant_ID training_network
@@ -30,18 +33,18 @@ quantum net-create --tenant-id $Tenant_ID training_network
 # Add Subnet
 quantum subnet-create --tenant-id $Tenant_ID training_network 25.25.25.0/24
 echo "Enter Subnet ID"
-read $Subnet_ID
+read Subnet_ID
 
 # Create Router
 quantum router-create --tenant-id $Tenant_ID training_router
 echo "Enter Router ID"
-read $training_router
+read training_router
 
 # Add Router to L3 Agent
-quantum agent-list (to get the l3 agent ID)
+quantum agent-list # to get the l3 agent ID
 echo "Enter L3 agent ID"
-read $l3_agent_ID
-quantum l3-agent-router-add $l3_agent_ID training_router
+read l3_agent_ID
+quantum l3-agent-router-add $l3_agent_ID $training_router
 
 # Add Router To Subnet
 quantum router-interface-add $training_router $Subnet_ID
