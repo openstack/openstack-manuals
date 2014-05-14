7a32d98452c690268fcc2038b5b92cb414708800
Andreas Jaeger <aj@suse.de>
2014-05-13 20:26:50 -0400

    Add translation test gate
    Test that the translated manuals can actually be build. This tests all
    currently supported manuals for building so that we can see which books
    currently built - and which fail.
    
    Change-Id: Ia867e94511092cb638493634ea1f7b7e417a5c52

---
+++ tools/test-languages.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function setup_directory {
+    SET_LANG=$1
+    shift
+    for BOOK_DIR in "$@" ; do
+        openstack-generate-docbook -l $SET_LANG -b $BOOK_DIR
+    done
+}
+
+
+function setup_lang {
+    SET_LANG=$1
+    shift
+    echo "Setting up files for $SET_LANG"
+    echo "======================="
+    setup_directory $SET_LANG 'common' 'glossary' "$@"
+    cp doc/pom.xml generated/$SET_LANG/pom.xml
+}
+
+function test_manuals {
+    SET_LANG=$1
+    shift
+    for BOOK in "$@" ; do
+        echo "Building $BOOK for language $SET_LANG..."
+        setup_directory $SET_LANG $BOOK
+        openstack-doc-test --check-build -l $SET_LANG --only-book $BOOK
+        RET=$?
+        if [ "$RET" -eq "0" ] ; then
+            echo "... succeeded"
+        else
+            echo "... failed"
+            BUILD_FAIL=1
+        fi
+    done
+}
+
+function test_ja {
+    setup_lang 'ja' 'user-guide'
+    test_manuals 'ja' 'security-guide' 'high-availability-guide'
+}
+
+
+BUILD_FAIL=0
+test_ja
+
+exit $BUILD_FAIL
