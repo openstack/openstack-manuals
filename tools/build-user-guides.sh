c2cac00294c4192a9a68162789aacff20b51a07c
Andreas Jaeger <aj@suse.de>
2015-05-29 14:09:00 +0200

    Cleanup builds
    Remove separate build-user-guides.sh and create a new build-all-rst.sh
    tool.
    
    Remove networking tox environment, since the RST guides share files, we
    should always build them together using the docs tox environment.
    
    Change-Id: I7ef98ca5e134096e14c79d58b0fc62a07747e471

--- tools/build-user-guides.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-tools/build-rst.sh doc/user-guide --glossary --build build
-# No need to build the glossary again here.
-tools/build-rst.sh doc/user-guide-admin --build build
-tools/build-rst.sh doc/admin-guide-cloud-rst --build build
