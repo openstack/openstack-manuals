f3dc7772b3612a1a5961564d4c9ea469578a3b08
Christian Berendt <berendt@b1-systems.de>
2016-01-13 11:39:05 +0100

    Adds 2016 copyright year to sphinx conf files
    Change-Id: I7e1a5e408490f35ad859bfa9fd91335a3417d00a

--- doc/cli-ref-rst/source/conf.py
+++ doc/cli-ref-rst/source/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'Command-Line Interface Reference'
 bug_tag = u'cli-ref-rst'
-copyright = u'2015, OpenStack contributors'
+copyright = u'2015-2016, OpenStack contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
