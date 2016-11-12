9b0c74008a88539e3c490a94b48f0572ed64e99e
Andreas Jaeger <aj@suse.com>
2016-11-12 19:24:01 +0100

    Set up stable/newton branch
    Set up stable/branch properly:
    * Disable all non-translated and non-versioned guides for translation.
    * Only build backported guides (install-guide, config-reference,
      networking-guide).
    * Publish backported guides and their translations to /newton/.
    * Do not publish web pages.
    * Update .gitreview for the branch.
    
    Change-Id: I9e0729bafda930bc80e5c68b8e4cf15e7cb050ca

--- tools/build-all-rst.sh
+++ tools/build-all-rst.sh
@@ -9,18 +9,19 @@ if [[ $# > 0 ]] ; then
     fi
 fi
 
-for guide in admin-guide arch-design cli-reference contributor-guide \
-    ha-guide image-guide ops-guide user-guide; do
-    tools/build-rst.sh doc/$guide --build build \
-        --target $guide $LINKCHECK
-done
+# Do not build for stable/newton, these are only published from master
+#for guide in admin-guide arch-design cli-reference contributor-guide \
+#    ha-guide image-guide ops-guide user-guide; do
+#    tools/build-rst.sh doc/$guide --build build \
+#        --target $guide $LINKCHECK
+#done
 
 # Draft guides
 # This includes guides that we publish from stable branches
 # as versioned like the networking-guide.
-for guide in networking-guide arch-design-draft config-reference; do
+for guide in networking-guide config-reference; do
     tools/build-rst.sh doc/$guide --build build \
-        --target "draft/$guide" $LINKCHECK
+        --target "newton/$guide" $LINKCHECK
 done
 
 tools/build-install-guides-rst.sh $LINKCHECK
