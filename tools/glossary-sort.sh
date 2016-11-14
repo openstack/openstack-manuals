d4f58113f9f347a3f817fba2ad15ddb15c621ffc
Andreas Jaeger <aj@suse.com>
2016-11-14 16:33:49 +0100

    Test that glossary is sorted
    Test that glossary entries are sorted and fail the niceness test if not.
    
    Sort those entries that were not sorted.
    
    Change-Id: I40eec5e6132bd79899456de505406ac2fa4ca32f

---
+++ tools/glossary-sort.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -xe
+
+# Check that doc/common/glossary entries are alphabetized and prints
+# list of entries that should be sorted.
+
+export TMPDIR=`/bin/mktemp -d`
+trap "rm -rf $TMPDIR" EXIT
+
+pushd $TMPDIR
+GLOSSARY=$OLDPWD/doc/common/glossary.rst
+
+grep '^   [a-zA-Z]' $GLOSSARY > glossary_entries
+
+LC_ALL=C sort --ignore-case glossary_entries -o glossary_entries.sorted
+
+if ! diff glossary_entries glossary_entries.sorted > glossary_entries.diff; then
+    echo "The following entries should be alphabetized: "
+    cat glossary_entries.diff | grep -e '> '
+    exit 1
+else
+    echo "Glossary alphabetized."
+fi
+
+popd
