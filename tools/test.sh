3ad1662bfa2acb4c367168b276f8a94ee1302aef
Doug Hellmann <doug@doughellmann.com>
2017-06-19 16:05:59 -0400

    replace install-guide landing page contents with list of projects
    This patch includes links to the projects that were included directly in
    the guide or for which a project-specific guide was linked from the old
    content. We can add and remove projects easily.
    
    Change-Id: I1fec91b1bae3289781d51a2021f555f020480262
    Depends-On: Ia750cb049c0f53a234ea70ce1f2bbbb7a2aa9454
    Signed-off-by: Doug Hellmann <doug@doughellmann.com>

---
+++ tools/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xe
+
+.tox/checkbuild/bin/python tools/www-generator.py --source-directory www/ \
+        --output-directory publish-docs/www/
+
+rsync -a www/static/ publish-docs/www/
+# publish-docs/www-index.html is the trigger for openstack-indexpage
+# to include the file.
+#mv publish-docs/www/www-index.html publish-docs/www-index.html
