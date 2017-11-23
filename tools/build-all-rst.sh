7482052d5f7982c7b7497b48b6a75a45c3a76094
Andreas Jaeger <aj@suse.com>
2017-11-23 14:20:10 +0100

    Fix ZUUL_REFNAME
    Set up the variables the same way the post job is set up:
    Pass them in via environment in job definition.
    
    Pass ZUUL_ environment variables through to jobs.
    
    Note ZUUL_REFNAME is not set, use ZUUL_BRANCH instead.
    
    Change-Id: I5863a05384c9a48445d5ce52ccc508519c7c00f8
    (cherry picked from commit 21edbc931fe09eedf8fa4e219fde1b222c9bce93)

--- tools/build-all-rst.sh
+++ tools/build-all-rst.sh
@@ -8,14 +8,19 @@ if [[ $# > 0 ]] ; then
         LINKCHECK="$1"
     fi
 fi
+# Set this to a sensible value if not set by OpenStack CI.
+if [ -z "$ZUUL_BRANCH" ] ; then
+    ZUUL_BRANCH="stable/mitaka"
+fi
 
 for guide in user-guide admin-guide cli-reference; do
    tools/build-rst.sh doc/$guide --build build \
        --target "mitaka/$guide" $LINKCHECK
 done
 
-# This marker is needed for infra publishing
-MARKER_TEXT="Project: $ZUUL_PROJECT Ref: $ZUUL_REFNAME Build: $ZUUL_UUID"
+# This marker is needed for infra publishing.
+# Note for stable branches, this needs to be the top of each manual.
+MARKER_TEXT="Project: $ZUUL_PROJECT Ref: $ZUUL_BRANCH Build: $ZUUL_UUID Revision: $ZUUL_NEWREV"
 
 # Draft guides
 # This includes guides that we publish from stable branches
