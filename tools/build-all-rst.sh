05174679adf955700d050d3f5b55ff4793f8d603
Ian Y. Choi <ianyrchoi@gmail.com>
2017-11-28 23:49:42 +0900

    Fixes to correctly publish stable/mitaka documents
    Change-Id: I66b8fe121c3636812eccc14851cb9d056631dafe

--- tools/build-all-rst.sh
+++ tools/build-all-rst.sh
@@ -13,10 +13,11 @@ if [ -z "$ZUUL_BRANCH" ] ; then
     ZUUL_BRANCH="stable/mitaka"
 fi
 
-for guide in user-guide admin-guide cli-reference; do
-   tools/build-rst.sh doc/$guide --build build \
-       --target "mitaka/$guide" $LINKCHECK
-done
+# Do not build for stable/mitaka, these are only published from master
+#for guide in user-guide admin-guide cli-reference; do
+#   tools/build-rst.sh doc/$guide --build build \
+#       --target "mitaka/$guide" $LINKCHECK
+#done
 
 # This marker is needed for infra publishing.
 # Note for stable branches, this needs to be the top of each manual.
@@ -25,7 +26,7 @@ MARKER_TEXT="Project: $ZUUL_PROJECT Ref: $ZUUL_BRANCH Build: $ZUUL_UUID Revision
 # Draft guides
 # This includes guides that we publish from stable branches
 # as versioned like the networking-guide.
-for guide in networking-guide config-reference ; do
+for guide in user-guide admin-guide cli-reference networking-guide config-reference ; do
     tools/build-rst.sh doc/$guide --build build \
         --target "mitaka/$guide" $LINKCHECK
     echo $MARKER_TEXT > publish-docs/mitaka/$guide/.root-marker
