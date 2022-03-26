d813ea12da9288ce92d3ea470e1eed3d81a37e03
Elod Illes <elod.illes@est.tech>
2022-03-26 12:08:25 +0100

    [www] Transition Ussuri to extended maintenance
    This patch fixes Ussuri's state in docs.openstack.org as Ussuri has
    transitioned to Extended Maintenance long time ago (2021-11-12).
    
    Change-Id: I8f5aa3725dfc541381d5dc0b437591750b27af84

--- tools/www-generator.py
+++ tools/www-generator.py
@@ -64,7 +64,7 @@ SERIES_INFO = {
     'rocky': SeriesInfo(date='August 2018', status='extended-maintenance'),
     'stein': SeriesInfo(date='April 2019', status='extended-maintenance'),
     'train': SeriesInfo(date='October 2019', status='extended-maintenance'),
-    'ussuri': SeriesInfo(date='May 2020', status='maintained'),
+    'ussuri': SeriesInfo(date='May 2020', status='extended-maintenance'),
     'victoria': SeriesInfo(date='October 2020', status='maintained'),
     'wallaby': SeriesInfo(date='April 2021', status='maintained'),
     'xena': SeriesInfo(date='October 2021', status='maintained'),
