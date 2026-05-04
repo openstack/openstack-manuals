e7baafa7aa5a5dfa1763e42f58619bfd69112061
Elod Illes <elod.illes@est.tech>
2026-05-04 12:51:52 +0200

    [www] Set 2024.2 Dalmatian as End of Life
    2024.2 Dalmatian is not a SLURP release [1], hence it moves to End of
    Life at the end of its maintained state.
    
    [1] https://releases.openstack.org/#slurp
    
    Change-Id: I7053deceaf2ae678cd9afb389238aaeefbdd34bd
    Signed-off-by: Elod Illes <elod.illes@est.tech>

--- tools/www-generator.py
+++ tools/www-generator.py
@@ -76,7 +76,7 @@ SERIES_INFO = collections.OrderedDict([
     ('2023.1', SeriesInfo(date='March 2023', status='unmaintained')),
     ('2023.2', SeriesInfo(date='October 2023', status='EOL')),
     ('2024.1', SeriesInfo(date='April 2024', status='unmaintained')),
-    ('2024.2', SeriesInfo(date='October 2024', status='maintained')),
+    ('2024.2', SeriesInfo(date='October 2024', status='EOL')),
     ('2025.1', SeriesInfo(date='April 2025', status='maintained')),
     ('2025.2', SeriesInfo(date='October 2025', status='maintained')),
     ('2026.1', SeriesInfo(date='April 2026', status='maintained')),
